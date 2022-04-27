7a68eff447178ca480ae6f03284fa78eb96a670d
gopireddysivakumar <104374267+gopireddysivakumar@users.noreply.github.com>
2022-04-27 15:35:45 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region     = "ap-south-1"
-  access_key = "AKIA2BI26NXQLCU3O6MDL"
-  secret_key = "Lz21PQpIt4yKTvpcQtHMqpq+5Fjf+KXzqAhO35nX"
+  region     = "us-west-1"
+  access_key = "AKIA5PWXUEEM7W4NV7T3"
+  secret_key = "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"
 }
